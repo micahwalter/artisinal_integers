c56eca1011ec27040ec9e4e6a349deafd348be47
Micah Walter <micah@micahwalter.com>
2020-05-27 21:01:22 -0400

    description data

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='artisinal_integers',
-      version='0.2',
-      description='get a unique artisinal integer',
+      version='0.2.1',
+      description='Get unique artisinal integers, locally sourced!',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='http://github.com/micahwalter/artisinal_integers',
       author='Micah Walter',
       author_email='micah@micahwalter.com',
